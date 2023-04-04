9c1609b41d6c362246053305a727410022b48c99
gskpsrikar <gskps2160@gmail.com>
2023-04-03 20:46:51 -0500

    inference-pipeline-refactoring

--- inference_pipeline.sh
+++ inference_pipeline.sh
@@ -2,3 +2,11 @@ cd inference_pipeline
 docker build -t my_flask_app .
 docker run -p 5000:5000 my_flask_app
 cd ..
+
+#aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 840807221296.dkr.ecr.us-east-1.amazonaws.com
+#cd inference_pipeline
+#docker build -t text_classifier_inference_image .
+#echo "docker build completed"
+#docker tag text_classifier_inference_image:latest 840807221296.dkr.ecr.us-east-1.amazonaws.com/text_classifier_inference_image:text_classifier_inference_image
+#docker push 840807221296.dkr.ecr.us-east-1.amazonaws.com/text_classifier_inference_image:text_classifier_inference_image
+#echo "pushing completed"
--- inference_pipeline/app.py
+++ inference_pipeline/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify, request
-from models import IncomingData, InferenceResult
+from application.models import IncomingData, InferenceResult
+from application.services import load_model
 
+model = load_model
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def health_check():
+    load_model()
     return jsonify({
-        'status': 'ok'
+        'status': 'ok',
+        'model': load_model()
     })
 
 
@@ -27,4 +31,11 @@ def post_mock_data():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    MODE = "local"
+
+    if MODE == "local":
+        app.run( port=5000, debug=True)
+    elif MODE == "local_container":
+        app.run(host="0.0.0.0", port=5000, debug=True)
+    elif MODE == "cloud_container":
+        pass
